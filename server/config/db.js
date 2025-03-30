67494a13c2bb66608429e1abb89b09899cfc5370
Kemal Gündoğdu <kemal.gundogdu20@ogr.atauni.edu.tr>
2025-03-30 23:49:51 +0300

    backend refactor

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB bağlantısı başarılı");
   } catch (err) {
     console.error("MongoDB bağlantı hatası:", err);
-    process.exit(1); // Hata durumunda uygulamayı kapat
+    process.exit(1);
   }
 };
 
-module.exports = connectDB
+module.exports = connectDB;
