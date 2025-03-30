60312b8ed8bfbd13544842de1990aff496e2c475
Kemal Gündoğdu <kemal.gundogdu20@ogr.atauni.edu.tr>
2025-03-31 00:11:08 +0300

    cors update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors()); // CORS middleware
+app.use(cors({
+  origin: ["https://www.kemalgundogdu.dev"], // İzin verilen origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // İzin verilen HTTP metodları
+  allowedHeaders: ["Content-Type", "Authorization"], // İzin verilen header'lar
+}));
 app.use(express.json());
 
 // Database Connection
